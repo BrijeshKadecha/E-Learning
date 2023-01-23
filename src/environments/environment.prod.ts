855981a56bd7ae3d549f6ce6bcd2e1dbbe21a8f0
Attia Imeed <attia.imeed@gmail.com>
2023-01-23 16:34:49 +0100

    Env Configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCCovTfcBsA1ek9h3ucOG8DnuVKMa9f6O4",
+    authDomain: "angular-firebase-attia-imed.firebaseapp.com",
+    databaseURL: "https://angular-firebase-attia-imed-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angular-firebase-attia-imed",
+    storageBucket: "angular-firebase-attia-imed.appspot.com",
+    messagingSenderId: "606451779781",
+    appId: "1:606451779781:web:11ba47e3bab6990aaa08ac",
+    measurementId: "G-7L2JRYK070"
+  }
 };
